b5f4e0797809b2bc1a0fd4c1bb25b55882864f1e
Brin29 <breinerstivenparracortes50@gmail.com>
2026-06-08 15:20:37 -0500

    ajuste

--- src/components/Layout/PublicLayout.tsx
+++ src/components/Layout/PublicLayout.tsx
@@ -1,9 +1,28 @@
 import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export const PublicLayout = () => {
+  const authUserStr = JSON.parse(localStorage.getItem("authMe")!!);
+
+  if (authUserStr) {
+    const accessToken = authUserStr.access_token;
+    const refreshToken = authUserStr.refresh_token;
+
+    if (accessToken && refreshToken) {
+      return <Navigate to="/dashboard" replace/>;
+    }
+  }
+
   return (
-    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", flex: 1, width: "100%" }}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        width: "100%",
+      }}
+    >
       <Outlet />
     </Box>
   );
